efb492520103cee4d3b0b56f7216bd8a3168e7b9
GitTest <https://github.com/Mauri2027/GitTest.git>
2021-10-30 02:09:28 -0300

    se agrego scanner

--- HolaMundo/src/com/company/Main.java
+++ HolaMundo/src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        Scanner myObject = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("ingrese hola para continuar");
+        String Name = myObject.nextLine();  // Read user input
+        System.out.println("ingreso: " + Name);  // Output user input
     }
 }
