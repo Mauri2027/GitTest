64181c832485cfe5e180391878c9ed487eeed7a5
GitTest <https://github.com/Mauri2027/GitTest.git>
2021-10-30 02:16:00 -0300

    condicion para ingreso de usuario scanner

--- HolaMundo/src/com/company/Main.java
+++ HolaMundo/src/com/company/Main.java
@@ -7,6 +7,12 @@ public class Main {
         Scanner myObject = new Scanner(System.in);  // Create a Scanner object
         System.out.println("ingrese hola para continuar");
         String Name = myObject.nextLine();  // Read user input
-        System.out.println("ingreso: " + Name);  // Output user input
+        if (Name.equals("hola")){
+            System.out.println("ingreso: " + Name);  // Output user input
+        }else {
+            System.out.println("no se ingreso hola, se ingreso:  "+ Name);
+        }
+
+
     }
 }
